d9989f7f7d4ffef4924cd640ddcbeddecf1ea6f1
Abdenacer Hasnaoui <56637282+abdenacer1993@users.noreply.github.com>
2024-12-17 00:28:47 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,6 @@ app.use("/api/blogs", (req, res) => res.json(blogs));
 app.use("/api/works", works);
 app.use("/contact", require("./routes/emailRoutes"));
 
-// Start the server
-const port = 5000;
+// Start the server with dynamic port assignment
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
